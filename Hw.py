6418817cad3c3d9498a7fc8dd447c4b592e6991b
Tyree Jackson <tjackson4@live.com>
2017-08-01 00:44:05 +0000

    Hw.py

---
+++ Hw.py
@@ -0,0 +1,290 @@
+import sys
+count = 1
+while count <=6:
+  sys.stdout.write('  *  ')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=5:
+  sys.stdout.write('    *')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=5:
+  sys.stdout.write(' ')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=6:
+  sys.stdout.write('  *  ')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=5:
+  sys.stdout.write('    *')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=5:
+  sys.stdout.write(' ')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=6:
+  sys.stdout.write('  *  ')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=5:
+  sys.stdout.write('    *')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=5:
+  sys.stdout.write(' ')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=6:
+  sys.stdout.write('  *  ')
+  count = count + 1
+sys.stdout.flush()
+
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=5:
+  sys.stdout.write('    *')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=5:
+  sys.stdout.write(' ')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=6:
+  sys.stdout.write('  *  ')
+  count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=100:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=130:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=130:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=130:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=130:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=130:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=130:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=130:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
+
+import sys
+count = 1
+sys.stdout.write('\n')
+sys.stdout.flush()
+
+import sys
+count = 1
+while count <=130:
+ sys.stdout.write('=')
+ count = count + 1
+sys.stdout.flush()
